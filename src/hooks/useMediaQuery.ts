03fa8424712d520e2ee71f378cbb5d1ccebc407d
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2025-01-12 08:01:55 +0330

    first commit

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+const useMediaQuery=(query:string)=>{
+    const [matches,setMatches]=useState(false);
+    useEffect(()=>{
+const media=window.matchMedia(query);
+if(media.matches!==matches){
+    setMatches(media.matches)
+}
+const listener=()=>setMatches(media.matches)
+window.addEventListener('resize',listener)
+    }, [matches, query])
+    return matches
+}
+export default useMediaQuery
